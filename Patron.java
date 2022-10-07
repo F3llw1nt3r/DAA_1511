fdd2c901f3c1414ec7694c51dc6d9d5cc2fc653a
TheMand0 <75743896+F3llw1nt3r@users.noreply.github.com>
2022-10-07 16:50:37 -0500

    Add files via upload

---
+++ Patron.java
@@ -0,0 +1,310 @@
+package patron;
+public class Patron {
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+      char [] patron = {'1','0','1','0','1','0','1'};
+      char [] arreglo = {'1','1','1','0','1','0','0','1','1','0','1','1','1','0','1','0','0','0','1','1','0','0','1','1','1'
+      ,'1','0','0','0','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','1','0','0','0',
+      '1','1','0','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0',
+      '0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1',
+      '0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1',
+      '1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1',
+      '1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0',
+      '1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0',
+      '0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','1','0','0','0','1','1','0','1',
+      '0','0','0','1','1','1','1','1','0','1','0','1','0','1','0','1','0','0','1','1','1','1','0','0','0','1','1','0','0','0'};
+        if(patron[0]==arreglo[0]&&patron[1]==arreglo[1]&&patron[2]==arreglo[2]&&patron[3]==arreglo[3]
+           &&patron[4]==arreglo[4]&&patron[5]==arreglo[5]&&patron[6]==arreglo[6]){
+            System.out.println("El patron esta aqui en los caracteres 0-6 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los caracteres 0-6 del arreglo.");
+        }
+        if(patron[0]==arreglo[7]&&patron[1]==arreglo[8]&&patron[2]==arreglo[9]&&patron[3]==arreglo[10]
+           &&patron[4]==arreglo[11]&&patron[5]==arreglo[12]&&patron[6]==arreglo[13]){
+            System.out.println("El patron esta aqui en los caracteres 7-13 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 7-13 del arreglo.");
+        }
+        if(patron[0]==arreglo[14]&&patron[1]==arreglo[15]&&patron[2]==arreglo[16]&&patron[3]==arreglo[17]
+           &&patron[4]==arreglo[18]&&patron[5]==arreglo[19]&&patron[6]==arreglo[20]){
+            System.out.println("El patron esta aqui en los caracteres 14-20 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 14-20 del arreglo.");
+        }
+        if(patron[0]==arreglo[21]&&patron[1]==arreglo[22]&&patron[2]==arreglo[23]&&patron[3]==arreglo[24]
+           &&patron[4]==arreglo[25]&&patron[5]==arreglo[26]&&patron[6]==arreglo[27]){
+            System.out.println("El patron esta aqui en los caracteres 21-27 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 21-27 del arreglo.");
+        }
+        if(patron[0]==arreglo[28]&&patron[1]==arreglo[29]&&patron[2]==arreglo[30]&&patron[3]==arreglo[31]
+           &&patron[4]==arreglo[32]&&patron[5]==arreglo[33]&&patron[6]==arreglo[34]){
+            System.out.println("El patron esta aqui en los caracteres 27-34 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 27-34 del arreglo.");
+        }
+        if(patron[0]==arreglo[35]&&patron[1]==arreglo[36]&&patron[2]==arreglo[37]&&patron[3]==arreglo[38]
+           &&patron[4]==arreglo[39]&&patron[5]==arreglo[40]&&patron[6]==arreglo[41]){
+            System.out.println("El patron esta aqui en los caracteres 35-41 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 35-41 del arreglo.");
+        }
+        if(patron[0]==arreglo[42]&&patron[1]==arreglo[43]&&patron[2]==arreglo[44]&&patron[3]==arreglo[45]
+           &&patron[4]==arreglo[46]&&patron[5]==arreglo[47]&&patron[6]==arreglo[48]){
+            System.out.println("El patron esta aqui en los caracteres 42-48 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 42-48 del arreglo.");
+        }
+        if(patron[0]==arreglo[49]&&patron[1]==arreglo[50]&&patron[2]==arreglo[51]&&patron[3]==arreglo[52]
+           &&patron[4]==arreglo[53]&&patron[5]==arreglo[54]&&patron[6]==arreglo[55]){
+            System.out.println("El patron esta aqui en los caracteres 49-55 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 49-55 del arreglo.");
+        }
+        if(patron[0]==arreglo[56]&&patron[1]==arreglo[57]&&patron[2]==arreglo[58]&&patron[3]==arreglo[59]
+           &&patron[4]==arreglo[60]&&patron[5]==arreglo[61]&&patron[6]==arreglo[62]){
+            System.out.println("El patron esta aqui en los caracteres 56-62 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 56-62 del arreglo.");
+        }
+        if(patron[0]==arreglo[63]&&patron[1]==arreglo[64]&&patron[2]==arreglo[65]&&patron[3]==arreglo[66]
+           &&patron[4]==arreglo[67]&&patron[5]==arreglo[68]&&patron[6]==arreglo[69]){
+            System.out.println("El patron esta aqui en los caracteres 62-69 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 62-69 del arreglo.");
+        }
+        if(patron[0]==arreglo[70]&&patron[1]==arreglo[71]&&patron[2]==arreglo[72]&&patron[3]==arreglo[73]
+           &&patron[4]==arreglo[74]&&patron[5]==arreglo[75]&&patron[6]==arreglo[76]){
+            System.out.println("El patron esta aqui en los caracteres 70-76 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 70-76 del arreglo.");
+        }
+        if(patron[0]==arreglo[77]&&patron[1]==arreglo[78]&&patron[2]==arreglo[79]&&patron[3]==arreglo[80]
+           &&patron[4]==arreglo[81]&&patron[5]==arreglo[82]&&patron[6]==arreglo[83]){
+            System.out.println("El patron esta aqui en los caracteres 77-83 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 77-83 del arreglo.");
+        }
+        if(patron[0]==arreglo[84]&&patron[1]==arreglo[85]&&patron[2]==arreglo[86]&&patron[3]==arreglo[87]
+           &&patron[4]==arreglo[88]&&patron[5]==arreglo[89]&&patron[6]==arreglo[90]){
+            System.out.println("El patron esta aqui en los caracteres 84-90 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 84-90 del arreglo.");
+        }
+        if(patron[0]==arreglo[91]&&patron[1]==arreglo[92]&&patron[2]==arreglo[93]&&patron[3]==arreglo[94]
+           &&patron[4]==arreglo[95]&&patron[5]==arreglo[96]&&patron[6]==arreglo[97]){
+            System.out.println("El patron esta aqui en los caracteres 91-97 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 91-97 del arreglo.");
+        }
+        if(patron[0]==arreglo[98]&&patron[1]==arreglo[99]&&patron[2]==arreglo[100]&&patron[3]==arreglo[101]
+           &&patron[4]==arreglo[102]&&patron[5]==arreglo[103]&&patron[6]==arreglo[104]){
+            System.out.println("El patron esta aqui en los caracteres 97-104 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 97-104 del arreglo.");
+        }
+         if(patron[0]==arreglo[105]&&patron[1]==arreglo[106]&&patron[2]==arreglo[107]&&patron[3]==arreglo[108]
+           &&patron[4]==arreglo[109]&&patron[5]==arreglo[110]&&patron[6]==arreglo[111]){
+            System.out.println("El patron esta aqui en los caracteres 105-111 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 105-111 del arreglo.");
+        }
+          if(patron[0]==arreglo[112]&&patron[1]==arreglo[113]&&patron[2]==arreglo[114]&&patron[3]==arreglo[115]
+           &&patron[4]==arreglo[116]&&patron[5]==arreglo[117]&&patron[6]==arreglo[118]){
+            System.out.println("El patron esta aqui en los caracteres 111-118 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 111-118 del arreglo.");
+        }
+           if(patron[0]==arreglo[119]&&patron[1]==arreglo[120]&&patron[2]==arreglo[121]&&patron[3]==arreglo[122]
+           &&patron[4]==arreglo[123]&&patron[5]==arreglo[124]&&patron[6]==arreglo[125]){
+            System.out.println("El patron esta aqui en los caracteres 118-125 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 118-125 del arreglo.");
+        }
+            if(patron[0]==arreglo[126]&&patron[1]==arreglo[127]&&patron[2]==arreglo[128]&&patron[3]==arreglo[129]
+           &&patron[4]==arreglo[130]&&patron[5]==arreglo[131]&&patron[6]==arreglo[132]){
+            System.out.println("El patron esta aqui en los caracteres 125-132 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 125-132 del arreglo.");
+        }
+             if(patron[0]==arreglo[133]&&patron[1]==arreglo[134]&&patron[2]==arreglo[135]&&patron[3]==arreglo[136]
+           &&patron[4]==arreglo[137]&&patron[5]==arreglo[138]&&patron[6]==arreglo[139]){
+            System.out.println("El patron esta aqui en los caracteres 132-139 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 132-139 del arreglo.");
+        }
+         if(patron[0]==arreglo[140]&&patron[1]==arreglo[141]&&patron[2]==arreglo[142]&&patron[3]==arreglo[143]
+           &&patron[4]==arreglo[144]&&patron[5]==arreglo[145]&&patron[6]==arreglo[146]){
+            System.out.println("El patron esta aqui en los caracteres 139-146 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 139-146 del arreglo.");
+        }
+         if(patron[0]==arreglo[147]&&patron[1]==arreglo[148]&&patron[2]==arreglo[149]&&patron[3]==arreglo[150]
+           &&patron[4]==arreglo[151]&&patron[5]==arreglo[152]&&patron[6]==arreglo[153]){
+            System.out.println("El patron esta aqui en los caracteres 146-153 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 146-153 del arreglo.");
+        }
+         if(patron[0]==arreglo[154]&&patron[1]==arreglo[155]&&patron[2]==arreglo[156]&&patron[3]==arreglo[157]
+           &&patron[4]==arreglo[158]&&patron[5]==arreglo[159]&&patron[6]==arreglo[160]){
+            System.out.println("El patron esta aqui en los caracteres 154-160 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 154-160 del arreglo.");
+        }
+         if(patron[0]==arreglo[161]&&patron[1]==arreglo[162]&&patron[2]==arreglo[163]&&patron[3]==arreglo[164]
+           &&patron[4]==arreglo[165]&&patron[5]==arreglo[166]&&patron[6]==arreglo[167]){
+            System.out.println("El patron esta aqui en los caracteres 161-167 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 161-167 del arreglo.");
+        }
+         if(patron[0]==arreglo[168]&&patron[1]==arreglo[169]&&patron[2]==arreglo[170]&&patron[3]==arreglo[171]
+           &&patron[4]==arreglo[172]&&patron[5]==arreglo[173]&&patron[6]==arreglo[174]){
+            System.out.println("El patron esta aqui en los caracteres 168-174 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 168-174 del arreglo.");
+        }
+         if(patron[0]==arreglo[175]&&patron[1]==arreglo[176]&&patron[2]==arreglo[177]&&patron[3]==arreglo[178]
+           &&patron[4]==arreglo[179]&&patron[5]==arreglo[180]&&patron[6]==arreglo[181]){
+            System.out.println("El patron esta aqui en los caracteres 175-181 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 175-181 del arreglo.");
+        }
+         if(patron[0]==arreglo[182]&&patron[1]==arreglo[183]&&patron[2]==arreglo[184]&&patron[3]==arreglo[185]
+           &&patron[4]==arreglo[186]&&patron[5]==arreglo[187]&&patron[6]==arreglo[188]){
+            System.out.println("El patron esta aqui en los caracteres 182-188 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 182-188 del arreglo.");
+        }
+         if(patron[0]==arreglo[189]&&patron[1]==arreglo[190]&&patron[2]==arreglo[191]&&patron[3]==arreglo[192]
+           &&patron[4]==arreglo[193]&&patron[5]==arreglo[194]&&patron[6]==arreglo[195]){
+            System.out.println("El patron esta aqui en los caracteres 189-195 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 189-195 del arreglo.");
+        }
+         if(patron[0]==arreglo[196]&&patron[1]==arreglo[197]&&patron[2]==arreglo[198]&&patron[3]==arreglo[199]
+           &&patron[4]==arreglo[200]&&patron[5]==arreglo[201]&&patron[6]==arreglo[202]){
+            System.out.println("El patron esta aqui en los caracteres 196-202 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 196-202 del arreglo.");
+        }
+         if(patron[0]==arreglo[203]&&patron[1]==arreglo[204]&&patron[2]==arreglo[205]&&patron[3]==arreglo[206]
+           &&patron[4]==arreglo[207]&&patron[5]==arreglo[208]&&patron[6]==arreglo[209]){
+            System.out.println("El patron esta aqui en los caracteres 203-209 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 203-209 del arreglo.");
+        }
+         if(patron[0]==arreglo[210]&&patron[1]==arreglo[211]&&patron[2]==arreglo[212]&&patron[3]==arreglo[213]
+           &&patron[4]==arreglo[214]&&patron[5]==arreglo[215]&&patron[6]==arreglo[216]){
+            System.out.println("El patron esta aqui en los caracteres 210-216 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 210-216 del arreglo.");
+        }
+         if(patron[0]==arreglo[217]&&patron[1]==arreglo[218]&&patron[2]==arreglo[219]&&patron[3]==arreglo[220]
+           &&patron[4]==arreglo[221]&&patron[5]==arreglo[222]&&patron[6]==arreglo[223]){
+            System.out.println("El patron esta aqui en los caracteres 217-223 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 217-223 del arreglo.");
+        }
+         if(patron[0]==arreglo[224]&&patron[1]==arreglo[225]&&patron[2]==arreglo[226]&&patron[3]==arreglo[227]
+           &&patron[4]==arreglo[228]&&patron[5]==arreglo[229]&&patron[6]==arreglo[230]){
+            System.out.println("El patron esta aqui en los caracteres 224-230 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 224-230 del arreglo.");
+        }
+         if(patron[0]==arreglo[231]&&patron[1]==arreglo[232]&&patron[2]==arreglo[233]&&patron[3]==arreglo[234]
+           &&patron[4]==arreglo[235]&&patron[5]==arreglo[236]&&patron[6]==arreglo[237]){
+            System.out.println("El patron esta aqui en los caracteres 231-237 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 231-237 del arreglo.");
+        }
+         if(patron[0]==arreglo[238]&&patron[1]==arreglo[239]&&patron[2]==arreglo[240]&&patron[3]==arreglo[241]
+           &&patron[4]==arreglo[242]&&patron[5]==arreglo[243]&&patron[6]==arreglo[244]){
+            System.out.println("El patron esta aqui en los caracteres 238-244 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 238-244 del arreglo.");
+        }
+         if(patron[0]==arreglo[245]&&patron[1]==arreglo[246]&&patron[2]==arreglo[247]&&patron[3]==arreglo[248]
+           &&patron[4]==arreglo[249]&&patron[5]==arreglo[250]&&patron[6]==arreglo[251]){
+            System.out.println("El patron esta aqui en los caracteres 245-251 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 245-251 del arreglo.");
+        }
+         if(patron[0]==arreglo[252]&&patron[1]==arreglo[253]&&patron[2]==arreglo[254]&&patron[3]==arreglo[255]
+           &&patron[4]==arreglo[256]&&patron[5]==arreglo[257]&&patron[6]==arreglo[258]){
+            System.out.println("El patron esta aqui en los caracteres 252-258 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 252-258 del arreglo.");
+        }
+         if(patron[0]==arreglo[259]&&patron[1]==arreglo[260]&&patron[2]==arreglo[261]&&patron[3]==arreglo[262]
+           &&patron[4]==arreglo[263]&&patron[5]==arreglo[264]&&patron[6]==arreglo[265]){
+            System.out.println("El patron esta aqui en los caracteres 259-265 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 259-265 del arreglo.");
+        }
+         if(patron[0]==arreglo[266]&&patron[1]==arreglo[267]&&patron[2]==arreglo[268]&&patron[3]==arreglo[269]
+           &&patron[4]==arreglo[270]&&patron[5]==arreglo[271]&&patron[6]==arreglo[272]){
+            System.out.println("El patron esta aqui en los caracteres 266-272 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 266-272 del arreglo.");
+        }
+         if(patron[0]==arreglo[273]&&patron[1]==arreglo[274]&&patron[2]==arreglo[275]&&patron[3]==arreglo[276]
+           &&patron[4]==arreglo[277]&&patron[5]==arreglo[278]&&patron[6]==arreglo[279]){
+            System.out.println("El patron esta aqui en los caracteres 273-279 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 273-279 del arreglo.");
+        }
+         if(patron[0]==arreglo[280]&&patron[1]==arreglo[281]&&patron[2]==arreglo[282]&&patron[3]==arreglo[283]
+           &&patron[4]==arreglo[284]&&patron[5]==arreglo[285]&&patron[6]==arreglo[286]){
+            System.out.println("El patron esta aqui en los caracteres 280-286 del arreglo.");
+        }
+        else{
+            System.out.println("El patron no esta en los carateres 289-286 del arreglo.");
+        }
+        System.out.println("El arreglo solo tiene 288 caracteres es inutil seguir buscando");
+        System.out.println("Además el patron ya fue encontrado en la posición 266 a 272");
+        System.out.println("El patron es: ");
+        System.out.println(patron[0]+""+patron[1]+""+patron[2]+""+patron[3]+""+patron[4]+""+patron[5]+""+patron[6]);
+        System.out.println("Y esta de la posición 266 a la posición 272 del arreglo: ");
+        System.out.println(arreglo[266]+""+arreglo[267]+""+arreglo[268]+""+arreglo[269]+""+arreglo[270]+""+arreglo[271]+""+arreglo[272]);
+       }
+    } 
